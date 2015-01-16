9ba6a3f1a718ea097f26fd76993f5d5997fda9bc
carlosazaustre <cazaustre@gmail.com>
2015-01-16 10:31:59 +0100

    preparing tests to BDD

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hacktool
 Project about to start
+
+## Getting started
+
+```
+$ git clone https://github.com/HackathonLovers/hacktool.git && cd $_
+$ npm install
+```
+
+## Running Tests
+```
+$ npm start
+```
---
+++ test/user.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var request = require("request");
+var expect = require("chai").expect;
+
+describe("Users register and authentication [/auth]", function() {
+
+  it("should be create a new user");
+  it("should be authenticate an existing user");
+
+});
