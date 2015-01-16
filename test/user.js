8decc97105ac82f168e621798015e47f7c795fa5
carlosazaustre <cazaustre@gmail.com>
2015-01-17 00:31:20 +0100

    add user module

--- test/user.js
+++ test/user.js
@@ -2,8 +2,20 @@
 
 var request = require("request");
 var expect = require("chai").expect;
+var mongoose = require("mongoose");
 
 describe("Users register and authentication [/auth]", function() {
+
+  before(function(done) {
+    mongoose.connect('mongodb://localhost/hacktool-test', done);
+  });
+
+  after(function(done) {
+    mongoose.connection.collections['users'].drop(function(err) {
+      console.log('Test Database dropped');
+    });
+    mongoose.disconnect(done);
+  });
 
   it("should be create a new user");
   it("should be authenticate an existing user");
