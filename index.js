a7aeba85eaf5b9c3f08a9e22fba8280a868e4b49
carlosazaustre <cazaustre@gmail.com>
2015-01-18 14:37:46 +0100

    add /auth route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
 
 var express = require("express");
+var user = require("./lib/user");
 var app = express();
 
 app.get("/", function(req, res) {
   res.status(200).json({ message: "You rules man!" });
 });
+
+app.post("/auth/github", user.authController);
 
 app.listen(3000, function(err) {
   console.log("Express server running at http://localhost:3000");
